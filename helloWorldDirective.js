f641d4524da1c742dc9fb5b680c6138d48574bad
Matt Linaberry <mtlinaberry@gmail.com>
2015-12-23 12:10:28 -0500

    Finished!

--- helloWorldDirective.js
+++ helloWorldDirective.js
@@ -1,1 +1,9 @@
-
+(function() {
+    var app = angular.module('directivesModule', []);
+    
+    app.directive('helloWorld', function() {
+       return {
+           template: 'Hello world!'
+       } 
+    });
+}());
